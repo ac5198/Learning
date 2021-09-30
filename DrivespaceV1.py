e408fa3337439206c8fdb1ce5b1b94e24d17c724
ac5198 <65299684+ac5198@users.noreply.github.com>
2021-09-30 15:11:27 -0500

    Add files via upload

--- DrivespaceV1.py
+++ DrivespaceV1.py
@@ -1,28 +1,15 @@
-# Ask for drive size
-current = int(input('What is the current drive size in GB? '))
-
-# Ask for free space
-free = (int(input('How much free space is left on the drive in GB? ')))
-
-# Ask for amount free space wanted
+current = float(input('What is the current drive size in GB? '))
+free = (float(input('How much free space is left on the drive in GB? ')))
 percent = (float(input('What percent do you want free? ')))
+
 convertpercent = (percent / 100)
-
-#Calculate used space
 used = (current - free)
-
-print(' ')
-
-#Calculate desired drive size
 newpercent = (1 - convertpercent)
 newsize = (used / newpercent)
-print ('The new drive size will need to be ', newsize, ' GB')
 
-#Calculate how much to add
+print ('\nThe new drive size will need to be ', newsize, 'GB')
+
 add = (newsize - current)
 print ('You will need to add an additional ', add, 'GB')
 
-print(' ')
-
-k=input("press enter to exit")
-
+k=input("\npress enter to exit")
