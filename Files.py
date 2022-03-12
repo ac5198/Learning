0366c9b52523c736ff816a4b003d1315b4c255c6
acoughlin <coughlinap@gmail.com>
2022-03-12 15:51:12 -0600

    Files

--- Files.py
+++ Files.py
@@ -21,7 +21,13 @@ print(list(shelfFile.keys()))
 print(list(shelfFile.values()))
 
 
-# Section 11 32
+# Section 11 32 - copy and move files
 
-import shutil
-shutil.copy('/home/acoughlin/PycharmProjects/Learning/Helloworld2.txt', '/home/acoughlin/PycharmProjects/Learning2/)
+#import shutil
+#shutil.copy('/home/acoughlin/PycharmProjects/Learning/Helloworld2.txt', '/home/acoughlin/PycharmProjects/helloworldtest.txt')
+#shutil.move('/home/acoughlin/PycharmProjects/Learning/Helloworld2.txt', '/home/acoughlin/PycharmProjects')
+
+# Section 11 22 - Deleting FIles
+
+os.unlink('/home/acoughlin/PycharmProjects/Learning/Helloworld2.txt')
+os.rmdir('/home/acoughlin/PycharmProjects/testfolder')
